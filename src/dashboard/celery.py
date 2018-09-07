11caf8eeec14f6faa221ac09af03e147ebfc8072
ssivart <travishen.tw@gmail.com>
2018-09-07 14:49:39 +0800

    Modify celery tasks schedule
    Tracking 3 day and 2 weeks

--- src/dashboard/celery.py
+++ src/dashboard/celery.py
@@ -26,117 +26,117 @@ app.conf.beat_schedule = {
         'task': 'DefaultWatchlistMonitorProfileUpdate',
         'schedule': crontab(minute='*/15'),
     },
-    # ======================================== Daily Builder ========================================
-    'daily-chicken-builder-1d': {
+    # ======================================== 3 day Builder ========================================
+    'daily-chicken-builder-3d': {
         'task': 'DailyChickenBuilder',
         'schedule': crontab(minute=5, hour='*'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-ram-builder-1d': {
+    'daily-ram-builder-3d': {
         'task': 'DailyRamBuilder',
         'schedule': crontab(minute=15, hour='*'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-goose-builder-1d': {
+    'daily-goose-builder-3d': {
         'task': 'DailyGooseBuilder',
         'schedule': crontab(minute=25, hour='*'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-duck-builder-1d': {
+    'daily-duck-builder-3d': {
         'task': 'DailyDuckBuilder',
         'schedule': crontab(minute=35, hour='*'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-hog-builder-1d': {
+    'daily-hog-builder-3d': {
         'task': 'DailyHogBuilder',
         'schedule': crontab(minute=45, hour='*'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-rice-builder-1d': {
+    'daily-rice-builder-3d': {
         'task': 'DailyRiceBuilder',
         'schedule': crontab(minute=55, hour='*'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-flower-builder-1d': {
+    'daily-flower-builder-3d': {
         'task': 'DailyFlowerBuilder',
         'schedule': crontab(minute=0, hour='*'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-crop-builder-1d': {
+    'daily-crop-builder-3d': {
         'task': 'DailyCropBuilder',
         'schedule': crontab(minute=10, hour='0,3,6,9,12,15,18,21'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-fruit-builder-1d': {
+    'daily-fruit-builder-3d': {
         'task': 'DailyFruitBuilder',
         'schedule': crontab(minute=20, hour='1,4,7,10,13,16,19,22'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-seafood-builder-1d': {
+    'daily-seafood-builder-3d': {
         'task': 'DailySeafoodBuilder',
         'schedule': crontab(minute=40, hour='11,14'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    'daily-cattle-builder-1d': {
+    'daily-cattle-builder-3d': {
         'task': 'DailyCattleBuilder',
         'schedule': crontab(minute=10, hour='*'),
-        'args': (0,)  # direct today
+        'args': (-2,)  # direct 3 day
     },
-    # ======================================== Weekly Builder ========================================
-    'daily-chicken-builder-7d': {
+    # ======================================== 2 week Builder ========================================
+    'daily-chicken-builder-14d': {
         'task': 'DailyChickenBuilder',
         'schedule': crontab(minute=0, hour='0,11'),
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-ram-builder-7d': {
+    'daily-ram-builder-14d': {
         'task': 'DailyRamBuilder',
         'schedule': crontab(minute=0, hour='1,12'),
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-goose-builder-7d': {
+    'daily-goose-builder-14d': {
         'task': 'DailyGooseBuilder',
         'schedule': crontab(minute=0, hour='2,13'),
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-duck-builder-7d': {
+    'daily-duck-builder-14d': {
         'task': 'DailyDuckBuilder',
         'schedule': crontab(minute=0, hour='3,14'),
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-hog-builder-7d': {
+    'daily-hog-builder-14d': {
         'task': 'DailyHogBuilder',
         'schedule': crontab(minute=0, hour='4,15'),
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-rice-builder-7d': {
+    'daily-rice-builder-14d': {
         'task': 'DailyRiceBuilder',
         'schedule': crontab(minute=0, hour='5,16'),
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-flower-builder-7d': {
+    'daily-flower-builder-14d': {
         'task': 'DailyFlowerBuilder',
         'schedule': crontab(minute=0, hour='6,17'),
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-crop-builder-7d': {
+    'daily-crop-builder-14d': {
         'task': 'DailyCropBuilder',
         'schedule': crontab(minute=0, hour='7,18'),  # 2 hour
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-fruit-builder-7d': {
+    'daily-fruit-builder-14d': {
         'task': 'DailyFruitBuilder',
         'schedule': crontab(minute=0, hour='9,20'),  # 2 hour
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-seafood-builder-7d': {
+    'daily-seafood-builder-14d': {
         'task': 'DailySeafoodBuilder',
         'schedule': crontab(minute=0, hour='18,22'),  # 2 hour
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
-    'daily-cattle-builder-7d': {
+    'daily-cattle-builder-14d': {
         'task': 'DailyCattleBuilder',
         'schedule': crontab(minute=30, hour='0, 11'),
-        'args': (-7,)  # direct 7 days range
+        'args': (-13,)  # direct 14 days range
     },
     'beat-per-minute': {
         'task': 'Beat',
