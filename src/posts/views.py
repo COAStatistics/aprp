d0567d3c9f9e934c53d69f4e9eeaa6f977879194
MatsuiLin101 <matsuilin101@gmail.com>
2018-09-10 14:01:40 +0800

    List all post at socialwall page
    SocialWall list all post content with masonry layout.

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,21 @@
+from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template.loader import render_to_string
 
 from . import models
 from . import forms
 
+import json
+import requests
+
 
 def post_socialwall(request):
-    return HttpResponse('Hi')
+
+    url = 'http://localhost:8000/posts/api/all'
+    res = requests.get(url)
+    data = json.loads(res.content)
+    post_list = data
+
+    form = forms.PostForm()
+
+    return render(request, 'socialwall.html', locals())
