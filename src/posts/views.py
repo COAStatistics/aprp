3754d8b1a5669af7e363b80cc77de090c403e35e
MatsuiLin101 <matsuilin101@gmail.com>
2018-09-21 15:39:07 +0800

    SocialWall Beta
    Comment finish.
    Add post filter function.
    Create post btn move to right bottom.
    New a btn scroll screen to top.
    If search nothing, show no data page.
    Add search url and view.
    List post method use model objects instead api.

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template.loader import render_to_string
+from django.db.models import Q
 
+from comments.models import Comment
 from . import models
 from . import forms
 
@@ -11,11 +13,7 @@ import requests
 
 def post_socialwall(request):
 
-    url = 'http://localhost:8000/posts/api/'
-    res = requests.get(url)
-    data = json.loads(res.content)
-    post_list = data
-
+    post_list = models.Post.objects.all()
     form = forms.PostForm()
 
     return render(request, 'socialwall.html', locals())
@@ -29,5 +27,60 @@ def post_new_form(request):
     return JsonResponse(html, safe=False)
 
 
-def post_edit_form(request):
-    pass
+def post_search(request):
+
+    key = request.GET.get('key')
+    q = request.GET.get('q')
+    posts = models.Post.objects.all()
+    # search_name = "search_{}".format(key)
+
+    if key == "Everything":
+        comments = Comment.objects.all()
+        comments = comments.filter(
+            Q(content__icontains=q)
+        )
+        post_list = []
+        for i in comments:
+            try:
+                if i.content_object.id not in post_list:
+                    post_list.append(i.content_object.id)
+            except:
+                pass
+        query = posts.filter(
+            Q(title__icontains=q) |
+            Q(content__icontains=q) |
+            Q(user__username__icontains=q) |
+            Q(id__in=post_list)
+        )
+    elif key == "Title":
+        query = posts.filter(
+            Q(title__icontains=q)
+        )
+    elif key == "Author":
+        query = posts.filter(
+            Q(user__username__icontains=q)
+        )
+    elif key == "Content":
+        query = posts.filter(
+            Q(content__icontains=q)
+        )
+    elif key == "Comment":
+        comments = Comment.objects.all()
+        comments = comments.filter(
+            Q(content__icontains=q)
+        )
+        post_list = []
+        for i in comments:
+            try:
+                if i.content_object.id not in post_list:
+                    post_list.append(i.content_object.id)
+            except:
+                pass
+        query = models.Post.objects.filter(id__in=post_list)
+
+    if query.count() == 0:
+        html = render_to_string('socialwall404.html', {'q': q})
+    else:
+        html = render_to_string('post_area.html', {'post_list': query}, request=request)
+
+    return HttpResponse(html)
