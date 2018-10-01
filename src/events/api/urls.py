dd908748b7f8c057aec49c0c58653211ee7ac228
ssivart <travishen.tw@gmail.com>
2018-10-01 11:10:46 +0800

    Modify events urls
    Add slash in patterns

--- src/events/api/urls.py
+++ src/events/api/urls.py
@@ -8,10 +8,10 @@ from .views import (
 
 
 urlpatterns = [
-    url(r'^eventtype/(?P<pk>\d+)$', EventTypeRetrieveUpdateDestroyAPIView.as_view(), name='api_eventtype_rud'),
-    url(r'^event/(?P<pk>\d+)$', EventRetrieveUpdateDestroyAPIView.as_view(), name='api_event_rud'),
-    url(r'^eventtype$', EventTypeListCreateAPIView.as_view(), name='api_eventtype_cr'),
-    url(r'^event$', EventListCreateAPIView.as_view(), name='api_event_cr'),
+    url(r'^eventtype/(?P<pk>\d+)/$', EventTypeRetrieveUpdateDestroyAPIView.as_view(), name='api_eventtype_rud'),
+    url(r'^event/(?P<pk>\d+)/$', EventRetrieveUpdateDestroyAPIView.as_view(), name='api_event_rud'),
+    url(r'^eventtype/$', EventTypeListCreateAPIView.as_view(), name='api_eventtype_cr'),
+    url(r'^event/$', EventListCreateAPIView.as_view(), name='api_event_cr'),
 ]
 
 
