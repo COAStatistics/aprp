dd4940b1c4c5ab6348815223d522ce82617bd574
travishen.tw <travishen.tw@gmail.com>
2018-12-13 11:41:51 +0800

    Add filter user admin and expose url

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import ugettext_lazy as _
 
 from .models import (ActivationProfile,
                      GroupInformation,
@@ -8,9 +11,52 @@ from .models import (ActivationProfile,
                      AbstractProfile)
 
 
+class GroupListFilter(SimpleListFilter):
+    title = _('Group')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'category'
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return [(group.id, group.name) for group in Group.objects.all()]
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or 'other')
+        # to decide how to filter the queryset.
+        if self.value():
+            return queryset.filter(user__groups__id=self.value())
+        else:
+            return queryset
+
+
+class UserInformationAdmin(admin.ModelAdmin):
+    model = UserInformation
+    list_filter = (
+        GroupListFilter,
+    )
+    search_fields = (
+        'id',
+        'user__first_name',
+        'user__last_name',
+        'user__username'
+    )
+
+
 admin.site.register(ActivationProfile)
 admin.site.register(GroupInformation)
-admin.site.register(UserInformation)
+admin.site.register(UserInformation, UserInformationAdmin)
 admin.site.register(ResetPasswordProfile)
 admin.site.register(ResetEmailProfile)
 admin.site.register(AbstractProfile)
--- src/dailytrans/tasks.py
+++ src/dailytrans/tasks.py
@@ -14,7 +14,8 @@ def delete_not_updated_trans(not_updated_times=15):
         dailytrans = DailyTran.objects.filter(not_updated__gte=not_updated_times)
         count = dailytrans.count()
         dailytrans.all().delete()
-        db_logger.info('Delete %s not updated dailytrans' % count, extra=logger_extra)
+        if count > 0:
+            db_logger.info('Delete %s not updated dailytrans' % count, extra=logger_extra)
 
     except Exception as e:
         db_logger.exception(e, extra=logger_extra)
