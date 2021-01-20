6395586cf958551edd7ad2e6b137f176473efd85
wadecym <wade.cym@msa.hinet.net>
2021-01-20 11:32:22 +0800

    Fix events api import file error
    fix events import wrong data format excel file raise django error

--- src/apps/events/api/utils.py
+++ src/apps/events/api/utils.py
@@ -2,7 +2,12 @@ import pandas as pd
 
 
 def convert_date(date):
-    date = date.split('/')
+    try:
+        date = date.split('/')
+    except:
+        #日期格式錯誤無法轉換
+        return None
+
     year = int(date[0])
     month = str(int(date[1])).zfill(2)
     day = str(int(date[-1])).zfill(2)
@@ -12,8 +17,23 @@ def convert_date(date):
 
 
 def funcBatchEventFile(file):
-    res = pd.read_excel(file)
-    res.columns = ['date', 'types', 'name', 'context']
-    res['date'] = res['date'].apply(convert_date)
+    #如果日期格式正確,但該事件不在事件分類內,會出現上傳成功但有錯誤筆數
+    try:
+        res = pd.read_excel(file)
+        res.columns = ['date', 'types', 'name', 'context']
+        res['date'] = res['date'].apply(convert_date)
 
-    return res.to_dict(orient='records')
+        #日期格式錯誤無法轉換
+        if res['date'].values[0] is None:
+            return None
+        
+        #格式正確
+        return res.to_dict(orient='records')
+
+    except:
+        #檔案非excel
+        #檔案內只有1列為標題,而沒有內容
+        #檔案內欄位數量錯誤
+        #檔案內有多列,但其中一列日期格式錯誤
+        return None
+
