1df93a12ba2e1241ac7daf7b56cc7957f1dd4485
wadecym <wade.cym@msa.hinet.net>
2022-02-07 09:09:32 +0800

    Update naifchickens add2chickens
    1. add item 白肉雞總貨 and 紅羽土雞總貨
    2. http request function from builder.py to naifchickens.py

--- src/apps/dailytrans/builders/naifchickens.py
+++ src/apps/dailytrans/builders/naifchickens.py
@@ -5,6 +5,15 @@ from .utils import date_transfer
 from .abstract import AbstractApi
 from apps.dailytrans.models import DailyTran
 
+import pandas as pd
+import numpy as np
+import os
+import time
+import requests
+import pytesseract
+from PIL import Image
+from bs4 import BeautifulSoup as bs
+from django.conf import settings
 
 class Api(AbstractApi):
 
@@ -13,10 +22,7 @@ class Api(AbstractApi):
     ZFILL = True
     ROC_FORMAT = False
     SEP = '/'
-
-    # Filters
-    DATE_FILTER = '$filter=date+like+%s'
-    CODE_FILTER = '$filter=item+like+%s'
+    headers = {'User-Agent': settings.USER_AGENT,}
 
     def __init__(self, model, config_code, type_id, logger_type_code=None):
         super(Api, self).__init__(model=model, config_code=config_code, type_id=type_id,
@@ -46,25 +52,265 @@ class Api(AbstractApi):
                                     % (product_code, dic), extra=self.LOGGER_EXTRA)
             return dic
 
-    def request(self, date=None, code=None):
-        url = self.API_URL
-        if date and code:
-            raise NotImplementedError('Do not support date and code advise as filter at one time!')
-        if date:
-            if not isinstance(date, datetime.date):
-                raise NotImplementedError
+    def request(self, start_date=None, end_date=None, id=None, source=None, code=None, name=None):
 
-            date_str = date_transfer(sep=self.SEP,
-                                     date=date,
-                                     roc_format=self.ROC_FORMAT,
-                                     zfill=self.ZFILL)
+        if id >= 150001 and id <= 150002:   #環南市場:環南-白肉雞/環南-土雞
+            login_fail_flag = False
+            #畜產會環南家禽批發市場交易行情表為一整個月份的數據,因此爬取網頁數據後組合成 json 格式以便後續流程
+            data_list = []
 
-            url = '&'.join((url, self.DATE_FILTER % date_str))
+            #pytesseract執行檔絕對路徑,系統需要額外安裝 tesseract-ocr
+            pytesseract.pytesseract.tesseract_cmd = settings.PYTESSERACT_PATH
 
-        if code:
-            url = '&'.join((url, self.CODE_FILTER % code))
+            #中央畜產會會員登入頁面
+            naif_login_url = settings.DAILYTRAN_BUILDER_API['naifchickens']
+            ss = requests.Session()
+            r1 = ss.get(naif_login_url, headers=self.headers)
 
-        return self.get(url)
+            #儲存驗證碼圖片
+            r2 = ss.get('https://www.naif.org.tw/libs/numToPic.aspx', headers=self.headers)
+            with open('captcha.jpg', 'wb') as f:
+                for chunk in r2:
+                    f.write(chunk)
+            
+            #OCR自動識別驗證碼
+            captcha = Image.open('captcha.jpg')
+            captcha_code = pytesseract.image_to_string(captcha).strip()
+            if len(captcha_code) > 4:
+                captcha_code = captcha_code[:4]
+
+            #登入會員系統
+            post_url = 'https://www.naif.org.tw/memberLogin.aspx'
+
+            postdata = {
+            'url': '/memberLogin.aspx',
+            'myAccount': settings.NAIF_ACCOUNT,
+            'myPassword': settings.NAIF_PASSWORD,
+            'code': captcha_code,
+            'btnSend': '登入會員',
+            'frontTitleMenuID': 105,
+            'frontMenuID': 148,
+            }
+
+            r3 = ss.post(post_url, headers=self.headers, data=postdata)
+
+            if start_date:
+                start_year = start_date.year
+                start_month = start_date.month
+            if end_date:
+                end_year = end_date.year
+                end_month = end_date.month
+
+            chickens_dict = {
+                        '120':'環南-白肉雞',
+                        '137':'環南-土雞',
+                    }   
+
+            for k,v in chickens_dict.items():
+
+                for y in range(start_year, end_year+1):
+                    if y == start_year and y == end_year:
+                        s_month = start_month
+                        e_month = end_month
+                    elif y == start_year:
+                        s_month = start_month
+                        e_month = 12
+                    elif y == end_year:
+                        s_month = 1
+                        e_month = end_month
+                    else:
+                        s_month = 1
+                        e_month = 12
+                    for m in range(s_month, e_month+1):
+                        if len(str(m)) == 1:
+                            m = '0' + str(m)
+
+                        #台北市環南家禽批發市場交易行情表頁面
+                        if k == '120':  #環南-白肉雞
+                            r4 = ss.get(f'https://www.naif.org.tw/infoWhiteChicken.aspx?sYear={y}&sMonth={m}&btnSearch=%E6%90%9C%E5%B0%8B&frontMenuID=120&frontTitleMenuID=37', headers=self.headers)
+                        if k == '137':   #環南-土雞
+                            r4 = ss.get(f'https://www.naif.org.tw/infoEndemicChicken.aspx?sYear={y}&sMonth={m}&btnSearch=%E6%90%9C%E5%B0%8B&frontMenuID=137&frontTitleMenuID=37', headers=self.headers)
+                        soup = bs(r4.text, 'html.parser')
+                        table = soup.find('table', {'cellspacing': '1'})
+                        alert = soup.find('script').string
+
+                        #登入失敗
+                        if alert and '請先登入會員!!' in alert:
+                            self.LOGGER.warning(f'Login failed for naif, captcha : {captcha_code}', extra=self.LOGGER_EXTRA)
+                            login_fail_flag = True
+                            break
+
+                        #登入成功
+                        if not alert and table:
+                            trs = table.find_all('tr')[1:]
+                            for tr in trs:
+
+                                #組合json格式用預設格式的
+                                default_dict1 = {
+                                                    "date": "",
+                                                    "item": v,
+                                                    "price": 0,
+                                                    "weight":0,
+                                                    "volume":0,
+                                                    "priceUnit": "元/公斤"
+                                                }
+                                
+                                d = tr.find('th').getText()
+                                tds = tr.find_all('td')
+
+                                avg_price = tds[4].getText().replace(",","").strip()
+                                volume = tds[6].getText().replace(",","").strip()
+                                weight = tds[7].getText().replace(",","").strip()
+                                if avg_price:
+                                    try:
+                                        avg_price = float(avg_price)
+                                    except ValueError:
+                                        self.LOGGER.warning(f'{y}/{m}/{d} {v} price Value Error : {tds[4].getText().replace(",","").strip()}', extra=self.LOGGER_EXTRA)
+                                        avg_price = None
+
+                                if volume:
+                                    try:
+                                        volume = float(volume)
+                                    except ValueError:
+                                        self.LOGGER.warning(f'{y}/{m}/{d} {v} volume Value Error : {tds[6].getText().replace(",","").strip()}', extra=self.LOGGER_EXTRA)
+                                        volume = None
+
+                                if weight:
+                                    try:
+                                        weight = float(weight)
+                                    except ValueError:
+                                        self.LOGGER.warning(f'{y}/{m}/{d} {v} weight Value Error : {tds[7].getText().replace(",","").strip()}', extra=self.LOGGER_EXTRA)
+                                        weight = None
+
+                                #組合成API可用的格式
+                                if avg_price and volume and weight:
+                                    default_dict1['date'] = f'{y}/{m}/{d}'
+                                    default_dict1['price'] = avg_price
+                                    default_dict1['volume'] = volume
+                                    default_dict1['weight'] = weight
+                                    data_list.append(default_dict1)
+
+                        time.sleep(5)
+
+                    #登入失敗時離開
+                    else:
+                        continue
+                    break
+            
+            #刪除驗證碼圖片檔案    
+            os.remove('captcha.jpg')
+
+            return data_list
+
+        elif id >= 150003 and id <= 150004: #家禽行情:白肉雞總貨/紅羽土雞總貨
+            data_list = []
+            # 將輸入的指定日期區間換成 list
+            date_range = pd.Series(pd.date_range(start=start_date, end=end_date))
+            for d in date_range:
+                date = d.strftime('%Y-%m-%d')
+            
+                naif_url = 'https://ppg.naif.org.tw/naif/MarketInformation/Poultry/Product.aspx'
+                not_data_flag = False   #判斷有無交易用的旗標
+
+                postdata = {
+                            '__VIEWSTATE:': None,
+                            '__VIEWSTATEGENERATOR': None,
+                            '__EVENTVALIDATION': None,
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$CheckBoxList_Market$6': '1',  # 肉品市場,台灣地區
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$CheckBoxList_Product$6': 'ON31',  # 白肉雞總貨
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$CheckBoxList_Product$8': 'ON41',  # 紅羽土雞雞總貨
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$TextBox_Content1_ThisDate': date,    # 日行情比較,本期
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$TextBox_Content1_LastDate': date,    # 日行情比較,上期
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$Button_Content1_Submit': '查詢',
+                            #
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content2_ThisDate_Year': '2022',    # 旬行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content2_ThisDate_Month': '1',  # 旬行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content2_ThisDate_TenDays': '1',    # 旬行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content2_LastDate_Year': '2022',    # 旬行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content2_LastDate_Month': '1',  # 旬行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content2_LastDate_TenDays': '1',    # 旬行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content3_ThisDate_Year': '2022',    # 月行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content3_ThisDate_Month': '1',  # 月行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content3_LastDate_Year': '2022',    # 月行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content3_LastDate_Month': '1',  # 月行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content4_ThisDate_Year': '2022',    # 年行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$DropDownList_Content4_LastDate_Year': '2022',    # 年行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$TextBox_Content5_ThisDate_BegDate': '2022-01-20',    # 指定期間行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$TextBox_Content5_ThisDate_EndDate': '2022-01-26',    # 指定期間行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$TextBox_Content5_LastDate_BegDate': '2022-01-20',    # 指定期間行情比較
+                            'ctl00$ctl00$ContentPlaceHolder_contant$ContentPlaceHolder_contant$TextBox_Content5_LastDate_EndDate': '2022-01-26'    # 指定期間行情比較
+                            }
+
+                # 組合json格式用預設格式
+                dict1 = {
+                        "date": "",
+                        "item": "白肉雞總貨",
+                        "price": 0,        
+                        "volume":0,
+                        "weight":0,
+                        "priceUnit": "元/公斤"
+                        }
+                dict2 = {
+                        "date": "",
+                        "item": "紅羽土雞總貨",
+                        "price": 0,        
+                        "volume":0,
+                        "weight":0,
+                        "priceUnit": "元/公斤"
+                        }
+                
+                ss = requests.Session()
+                r3 = ss.get(naif_url, headers=self.headers)
+                soup = bs(r3.text, 'html.parser')
+
+                postdata['__VIEWSTATE'] = soup.find(id='__VIEWSTATE').attrs['value']
+                postdata['__VIEWSTATEGENERATOR'] = soup.find(id='__VIEWSTATEGENERATOR').attrs['value']
+                postdata['__EVENTVALIDATION'] = soup.find(id='__EVENTVALIDATION').attrs['value']
+
+                r4 = ss.post(naif_url, headers=self.headers, data=postdata)
+                soup2 = bs(r4.text, 'html.parser')
+
+                table = soup2.find('table', {'cellspacing': '0'})
+                not_data_msg = table.find_all('tr')[3].getText().strip()   # 特殊日期沒有交易會顯示"查無交易資料!"
+                if '查無交易資料！' in not_data_msg:
+                    self.LOGGER.warning(f'{date} 查無交易資料！', extra=self.LOGGER_EXTRA)
+                    not_data_flag = True
+                    continue
+
+                trs = table.find_all('tr')[5:7]
+                if not not_data_flag:
+                    for tr in trs:
+                        tds = tr.find_all('td')
+                        name = tds[0].getText() + tds[1].getText()  #將"白肉雞"/"紅羽土雞"和"總貨"字串連接
+                        this_avg_price = tds[2].getText()
+                        # last_avg_price = tds[3].getText()
+                        this_volume = tds[5].getText()
+                        # last_volume = tds[6].getText()
+                        this_weight = tds[8].getText()
+                        # last_weight = tds[9].getText()
+
+                        #如果當天沒有數據
+                        if this_avg_price == '-':
+                            self.LOGGER.warning(f'{date} {name} 無交易數據', extra=self.LOGGER_EXTRA)
+                            continue
+
+                        # 白肉雞總貨和紅羽土雞總貨數據在同一頁面,判斷品項分開存資料
+                        if name in dict1['item']:
+                            dict1['date'] = date.replace('-','/')
+                            dict1['price'] = float(this_avg_price)
+                            dict1['volume'] = float(this_volume)
+                            dict1['weight'] = float(this_weight)
+                            data_list.append(dict1)
+                        if name in dict2['item']:
+                            dict2['date'] = date.replace('-','/')
+                            dict2['price'] = float(this_avg_price)
+                            dict2['volume'] = float(this_volume)
+                            dict2['weight'] = float(this_weight)
+                            data_list.append(dict2)
+
+                time.sleep(3)      
+
+            return data_list
 
     def load(self, response):
         data = []
--- src/apps/naifchickens/builder.py
+++ src/apps/naifchickens/builder.py
@@ -1,208 +1,41 @@
 from apps.dailytrans.builders.naifchickens import Api
 from apps.dailytrans.builders.utils import (
+    product_generator,
     director,
-    date_generator,
     DirectData,
-    date_delta,
 )
 from .models import Naifchickens
-import pandas as pd
-import numpy as np
-# from datetime import datetime,timedelta,date
-import time
-import os
-import requests
-import pytesseract
-from PIL import Image
-from django.conf import settings
-import re
-from bs4 import BeautifulSoup as bs
+from datetime import timedelta,date
 import json
-
-
-import logging
-db_logger = logging.getLogger('aprp')
 
 MODELS = [Naifchickens]
 CONFIG_CODE = 'COG16'
 LOGGER_TYPE_CODE = 'LOT-naifchickens'
-
-DELTA_DAYS = 1
-
+TYPE_ID = 1     # 1:批發; 2:產地; 3:零售
+DELTA_DAYS = 30
 
 @director
 def direct(start_date=None, end_date=None, *args, **kwargs):
 
-    data = DirectData(CONFIG_CODE, 1, LOGGER_TYPE_CODE)
+    data = DirectData(CONFIG_CODE, TYPE_ID, LOGGER_TYPE_CODE)
 
-    login_fail_flag = False
+    distinct_list = []
 
     for model in MODELS:
         api = Api(model=model, **data._asdict())
+        for obj in product_generator(model, type=TYPE_ID, **kwargs):
+            if not start_date:
+                start_date = (date.today() - timedelta(days=DELTA_DAYS)).strftime('%Y/%m/%d')
+            if not end_date:
+                end_date = date.today().strftime('%Y/%m/%d')
 
-        #畜產會環南家禽批發市場交易行情表為一整個月份的數據,因此爬取網頁數據後組合成 json 格式以便後續流程
-        data_list = []
+            # 白肉雞總貨和紅羽土雞總貨是同一網頁頁面,選其中一個,兩品項會一起抓,避免花時間重複抓
+            distinct_list.append(obj.id)
+            if (obj.id == 150004 and 150003 in distinct_list) or (obj.id == 150003 and 150004 in distinct_list):    
+                continue
 
-        #pytesseract執行檔絕對路徑,系統需要額外安裝 tesseract-ocr
-        pytesseract.pytesseract.tesseract_cmd = settings.PYTESSERACT_PATH
-
-        headers = {'User-Agent': settings.USER_AGENT,}
-
-        #中央畜產會會員登入頁面
-        naif_login_url = settings.DAILYTRAN_BUILDER_API['naifchickens']
-        ss = requests.Session()
-        r1 = ss.get(naif_login_url, headers=headers)
-
-        #儲存驗證碼圖片
-        r2 = ss.get('https://www.naif.org.tw/libs/numToPic.aspx', headers=headers)
-        with open('captcha.jpg', 'wb') as f:
-            for chunk in r2:
-                f.write(chunk)
-        
-        #OCR自動識別驗證碼
-        captcha = Image.open('captcha.jpg')
-        code = pytesseract.image_to_string(captcha).strip()
-        if len(code) > 4:
-            code = code[:4]
-
-        #登入會員系統
-        post_url = 'https://www.naif.org.tw/memberLogin.aspx'
-
-        postdata = {
-        'url': '/memberLogin.aspx',
-        'myAccount': settings.NAIF_ACCOUNT,
-        'myPassword': settings.NAIF_PASSWORD,
-        'code': code,
-        'btnSend': '登入會員',
-        'frontTitleMenuID': 105,
-        'frontMenuID': 148,
-        }
-
-        r3 = ss.post(post_url, headers=headers, data=postdata)
-
-        if start_date:
-            start_year = start_date.year
-            start_month = start_date.month
-        if end_date:
-            end_year = end_date.year
-            end_month = end_date.month
-
-        if not start_date and not end_date:
-            delta_start_date, delta_end_date = date_delta(DELTA_DAYS)
-            start_year = delta_start_date.year
-            start_month = delta_start_date.month
-            end_year = delta_end_date.year
-            end_month = delta_end_date.month 
-
-        chickens_dict = {
-                    '120':'環南-白肉雞',
-                    '137':'環南-土雞',
-                }   
-
-        for k,v in chickens_dict.items():
-
-            for y in range(start_year, end_year+1):
-                if y == start_year and y == end_year:
-                    s_month = start_month
-                    e_month = end_month
-                elif y == start_year:
-                    s_month = start_month
-                    e_month = 12
-                elif y == end_year:
-                    s_month = 1
-                    e_month = end_month
-                else:
-                    s_month = 1
-                    e_month = 12
-                for m in range(s_month, e_month+1):
-                    if len(str(m)) == 1:
-                        m = '0' + str(m)
-
-                    
-                    #台北市環南家禽批發市場交易行情表頁面
-                    if k == '120':  #環南_白肉雞
-                        r4 = ss.get(f'https://www.naif.org.tw/infoWhiteChicken.aspx?sYear={y}&sMonth={m}&btnSearch=%E6%90%9C%E5%B0%8B&frontMenuID=120&frontTitleMenuID=37', headers=headers)
-                    if k == '137':   #環南_土雞
-                        r4 = ss.get(f'https://www.naif.org.tw/infoEndemicChicken.aspx?sYear={y}&sMonth={m}&btnSearch=%E6%90%9C%E5%B0%8B&frontMenuID=137&frontTitleMenuID=37', headers=headers)
-                    soup = bs(r4.text, 'html.parser')
-                    table = soup.find('table', {'cellspacing': '1'})
-                    alert = soup.find('script').string
-
-                    #登入失敗
-                    if alert and '請先登入會員!!' in alert:
-                        db_logger.warning(f'Login failed for naif, captcha : {code}',extra={
-                                                                                                'logger_type': data.logger_type_code,
-                                                                                            })
-                        login_fail_flag = True
-                        break
-
-                    #登入成功
-                    if not alert and table:
-                        trs = table.find_all('tr')[1:]
-                        for tr in trs:
-
-                            #組合json格式用預設格式的
-                            default_dict1 = {
-                                                "date": "",
-                                                "item": v,
-                                                "price": 0,
-                                                "weight":0,
-                                                "volume":0,
-                                                "priceUnit": "元/公斤"
-                                            }
-                            
-                            d = tr.find('th').getText()
-                            tds = tr.find_all('td')
-
-                            avg_price = tds[4].getText().replace(",","").strip()
-                            volume = tds[6].getText().replace(",","").strip()
-                            weight = tds[7].getText().replace(",","").strip()
-                            if avg_price:
-                                try:
-                                    avg_price = float(avg_price)
-                                except ValueError:
-                                    db_logger.warning(f'{y}/{m}/{d} {v} price Value Error : {tds[4].getText().replace(",","").strip()}',extra={
-                                                                                                    'logger_type': data.logger_type_code,
-                                                                                                })
-                                    avg_price = None
-
-                            if volume:
-                                try:
-                                    volume = float(volume)
-                                except ValueError:
-                                    db_logger.warning(f'{y}/{m}/{d} {v} volume Value Error : {tds[6].getText().replace(",","").strip()}',extra={
-                                                                                                    'logger_type': data.logger_type_code,
-                                                                                                })
-                                    volume = None
-
-                            if weight:
-                                try:
-                                    weight = float(weight)
-                                except ValueError:
-                                    db_logger.warning(f'{y}/{m}/{d} {v} weight Value Error : {tds[7].getText().replace(",","").strip()}',extra={
-                                                                                                    'logger_type': data.logger_type_code,
-                                                                                                })
-                                    weight = None
-
-                            #組合成API可用的格式
-                            if avg_price and volume and weight:
-                                default_dict1['date'] = f'{y}/{m}/{d}'
-                                default_dict1['price'] = avg_price
-                                default_dict1['volume'] = volume
-                                default_dict1['weight'] = weight
-                                data_list.append(default_dict1)
-
-                    time.sleep(5)
-
-                #登入失敗時離開
-                else:
-                    continue
-                break
-
-        if not login_fail_flag:
-            api.load(json.dumps(data_list))
-
-            #刪除驗證碼圖片檔案    
-            os.remove('captcha.jpg')
+            response = api.request(start_date=start_date, end_date=end_date, id=obj.id)
+            if response:
+                api.load(json.dumps(response))
 
     return data
