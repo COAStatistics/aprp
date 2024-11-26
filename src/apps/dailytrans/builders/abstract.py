b4815f1b355ab949b21d9731475bbe2238026c52
WKHuang <asd0021128@gmail.com>
2024-11-26 09:04:30 +0800

    fix the api bug

--- src/apps/dailytrans/builders/abstract.py
+++ src/apps/dailytrans/builders/abstract.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from apps.configs.models import (
     Source,
     Config,
+    Type
 )
 
 
@@ -45,6 +46,7 @@ class AbstractApi(object):
 
 
         if type_id:
+            self.TYPE = Type.objects.get(id=type_id)
             self.SOURCE_QS = self.SOURCE_QS.filter(type__id=type_id)
             self.PRODUCT_QS = self.PRODUCT_QS.filter(type__id=type_id, track_item=True)
 
