5d4838ff1fc8bc7dc239e33d07e6f47f516bc6e1
Timy Lian <timy117@gmail.com>
2025-02-03 13:45:53 +0800

    Update admin.py
    Update admin page

--- src/apps/fruits/admin.py
+++ src/apps/fruits/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.forms import ModelForm
 from django.forms import TextInput
 from django.db import models
+from django.forms.utils import ErrorList
+
 from .models import Fruit
 
 
@@ -9,6 +11,27 @@ class FruitModelForm(ModelForm):
     class Meta:
         model = Fruit
         exclude = ['update_time']
+
+    def __init__(self, data=None, files=None, auto_id='id_%s', prefix=None, initial=None, error_class=ErrorList,
+                 label_suffix=None, empty_permitted=False, instance=None):
+        super().__init__(data, files, auto_id, prefix, initial, error_class, label_suffix, empty_permitted, instance)
+
+        self.fields['id'] = forms.IntegerField(widget=forms.TextInput())
+        self.fields['parent'].choices = self.parent_field_choices
+
+    @property
+    def parent_field_choices(self):
+        qs_products = Fruit.objects.all()
+
+        return [(obj.id, f"{obj.id} ({obj.name})") for obj in qs_products]
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        instance.id = self.cleaned_data['id']
+
+        if commit:
+            instance.save()
+        return instance
 
 
 class FruitAdmin(admin.ModelAdmin):
@@ -18,6 +41,13 @@ class FruitAdmin(admin.ModelAdmin):
     }
     list_display = ['id', 'name', 'code', 'type', 'parent', 'track_item', 'update_time']
     list_editable = ['name', 'code', 'type', 'parent', 'track_item']
+    fields = ['id', 'name', 'code', 'type', 'parent', 'track_item']
+
+    search_fields = (
+        'id',
+        'code',
+        'name',
+    )
 
 
 admin.site.register(Fruit, FruitAdmin)
