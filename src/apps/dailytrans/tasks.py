fc00ea423c805fd5979d7f851b0c704c838fdcf9
Timy Lian <timy117@gmail.com>
2025-02-07 10:04:17 +0800

    Update tasks.py
    Comment `delete_not_update_trans` function temporally

--- src/apps/dailytrans/tasks.py
+++ src/apps/dailytrans/tasks.py
@@ -11,19 +11,20 @@ from google_api.backends import DefaultGoogleDriveClient
 
 @task(name="DeleteNotUpdatedTrans")
 def delete_not_updated_trans(not_updated_times=15):
-    db_logger = logging.getLogger('aprp')
-    logger_extra = {
-        'type_code': 'LOT-dailytrans',
-    }
-    try:
-        dailytrans = DailyTran.objects.filter(not_updated__gte=not_updated_times)
-        count = dailytrans.count()
-        dailytrans.all().delete()
-        if count > 0:
-            db_logger.info('Delete %s not updated dailytrans' % count, extra=logger_extra)
+    pass
+    # db_logger = logging.getLogger('aprp')
+    # logger_extra = {
+    #     'type_code': 'LOT-dailytrans',
+    # }
+    # try:
+    #     dailytrans = DailyTran.objects.filter(not_updated__gte=not_updated_times)
+    #     count = dailytrans.count()
+    #     dailytrans.all().delete()
+    #     if count > 0:
+    #         db_logger.info('Delete %s not updated dailytrans' % count, extra=logger_extra)
 
-    except Exception as e:
-        db_logger.exception(e, extra=logger_extra)
+    # except Exception as e:
+    #     db_logger.exception(e, extra=logger_extra)
 
 
 @task(name='UpdateDailyReport')
